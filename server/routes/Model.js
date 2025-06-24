9b2c8077a04acaca0bba62e76f64c4f312a08918
vasudev <vasujoshi2003@gmail.com>
2025-06-25 00:31:15 +0530

    remove middleware to test model

--- server/routes/Model.js
+++ server/routes/Model.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const {getResponse } = require('../controllers/modelController');
 const {isLoggedIn} = require('../middlewares/Auth');
-router.post('/analyse',isLoggedIn,getResponse)
+router.post('/analyse',getResponse)
 module.exports = router;
